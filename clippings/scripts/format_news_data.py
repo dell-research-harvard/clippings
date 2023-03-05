a0753d0dfa17fd93a7ff2bb2a767ec6ea7530386
econabhishek <abhishek.arora1996@gmail.com>
2023-03-05 16:19:36 -0500

    test

--- clippings/scripts/format_news_data.py
+++ clippings/scripts/format_news_data.py
@@ -20,6 +20,17 @@ unlabelled_data = unlabelled_data.drop(columns=['Unnamed: 0'])
 unlabelled_data['label'] = 999
 ##Rename the columns
 unlabelled_data.columns = ['image_path', 'text', 'label']
+
+##Add base path to image path
+unlabelled_data['image_path'] = '/mnt/data02/captions/pulled_crops/' + unlabelled_data['image_path']
+
+##Split in val and train
+train, val = train_test_split(unlabelled_data, test_size=0.2, random_state=42)
+
+##Save the text data
+train.to_csv(f'texts/unlabelled_news_train_reformatted.csv', index=False)
+val.to_csv(f'texts/unlabelled_news_val_reformatted.csv', index=False)
+
 ###Save the text data
 unlabelled_data.to_csv(f'texts/unlabelled_news_reformatted.csv', index=False)
 
