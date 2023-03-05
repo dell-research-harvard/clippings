b1dcdd77ba1f03e14b297f47f59a8b437b632324
econabhishek <abhishek.arora1996@gmail.com>
2023-03-05 13:56:51 -0500

    test

--- clippings/scripts/format_food101_data.py
+++ clippings/scripts/format_food101_data.py
@@ -3,6 +3,8 @@
 import os
 import pandas as pd
 import numpy as np
+###Import test_train_split
+from sklearn.model_selection import train_test_split
 
 
 
@@ -16,8 +18,21 @@ def format_food101_data(split='train'):
     ###Append base paths to image paths
     text_data['image_path'] = '/mnt/data01/clippings_general/images/' +split +"/" + text_data['label'] +"/" + text_data['image_path']
 
-    ###Save the text data
-    text_data.to_csv(f'texts/{split}_titles_reformatted.csv', index=False)
+
+    ##Split into train and val if split=="train"
+    if split=="train":
+        ###Split into train and val
+        train, val = train_test_split(text_data, test_size=0.2, random_state=42)
+
+        ###Save the text data
+        train.to_csv(f'texts/train_titles_reformatted.csv', index=False)
+        val.to_csv(f'texts/val_titles_reformatted.csv', index=False)
+
+    else:
+        ###Save the text data
+        text_data.to_csv(f'texts/{split}_titles_reformatted.csv', index=False)
+
+
 
 
 
