376165bcf4fb0514502d9efbf3c6799a4dcd6153
econabhishek <abhishek.arora1996@gmail.com>
2023-03-07 19:24:16 -0500

    test

--- clippings/scripts/format_news_data.py
+++ clippings/scripts/format_news_data.py
@@ -45,7 +45,7 @@ import networkx as nx
 ##Load the json
 # train_data_path="/mnt/data01/clippings_general/texts/emily_news_captions_3.json"
 train_data_list=["/mnt/data01/clippings_general/texts/emily_news_captions_3.json","/mnt/data01/clippings_general/texts/emily_news_captions_2.json","/mnt/data01/clippings_general/texts/emily_newspaper_labels_0603.json"]
-train_data_list=["/mnt/data01/clippings_general/texts/emily_news_captions_3.json"]
+train_data_list=["/mnt/data01/clippings_general/texts/emily_news_captions_2.json"]
 
 ###Concat the jsons
 data = []
