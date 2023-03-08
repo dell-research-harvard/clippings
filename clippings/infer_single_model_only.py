d9710c4004a5f2d0d033649c9a1eadafb0cf5783
econabhishek <abhishek.arora1996@gmail.com>
2023-03-08 17:34:28 -0500

    test

--- clippings/infer_single_model_only.py
+++ clippings/infer_single_model_only.py
@@ -293,7 +293,7 @@ if __name__ == "__main__":
     if args.opt_im_wt:
         space = {
             "threshold":hp.uniform("threshold",0.01,1),
-            "im_wt":hp.uniform("im_wt",0,1),
+            "im_wt":hp.uniform("im_wt",0.3,1),
         }
     else:
 
