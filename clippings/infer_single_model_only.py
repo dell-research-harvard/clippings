d6a8c4b29a50b863a38e3f900aceb462f1f73ad3
econabhishek <abhishek.arora1996@gmail.com>
2023-03-08 17:00:17 -0500

    test

--- clippings/infer_single_model_only.py
+++ clippings/infer_single_model_only.py
@@ -288,7 +288,7 @@ if __name__ == "__main__":
         return -adjusted_rand_score(all_labels,clusters)
     
     space = {
-        "threshold":hp.uniform("threshold",0.01,0.5),
+        "threshold":hp.uniform("threshold",0.01,1),
         "im_wt":hp.uniform("im_wt",0,1),
     }
 
