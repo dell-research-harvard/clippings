ede111d9d8021cff632f8137150cadd38901f31b
econabhishek <abhishek.arora1996@gmail.com>
2023-03-07 23:56:44 -0500

    test

--- clippings/inference_clip.py
+++ clippings/inference_clip.py
@@ -265,7 +265,7 @@ if __name__ == "__main__":
     #             print("Path",all_paths[nn])
     #             print("Label",all_labels[nn])
 
-    print(cluster_assignment)
+    print(clusters)
 
     all_embeddings=all_embeddings.cpu().numpy()
     all_labels=all_labels.cpu().numpy()
