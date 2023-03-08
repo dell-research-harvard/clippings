d2adb3f8f3ed082e21051f43121e4db9413e5d42
econabhishek <abhishek.arora1996@gmail.com>
2023-03-08 18:18:27 -0500

    tets

--- clippings/infer_single_model_only.py
+++ clippings/infer_single_model_only.py
@@ -278,10 +278,14 @@ if __name__ == "__main__":
             all_embeddings=params["im_wt"]*all_image_embeddings + (1-params["im_wt"])*all_text_embeddings
         else:
             if args.im_wt==0:
+                print("using only text embeddings")
+
                 all_embeddings=all_text_embeddings
             elif args.im_wt==1:
+                print("using only image embeddings")
                 all_embeddings=all_image_embeddings
             else:
+                print("using weighted embeddings")
                 all_embeddings=args.im_wt*all_image_embeddings + (1-args.im_wt)*all_text_embeddings
             
         ##Normalize the embeddings
