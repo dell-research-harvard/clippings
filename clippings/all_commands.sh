0d82538574add44aaeac2ba2a23502d2222b6a1c
econabhishek <abhishek.arora1996@gmail.com>
2023-03-08 13:05:33 -0500

    test

--- clippings/all_commands.sh
+++ clippings/all_commands.sh
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=1 python clippings/continue_pretrain_clip.py --clip_lr 5e-9
 
 
 ####Contrastive loss
-CUDA_VISIBLE_DEVICES=0 python clippings/continue_pretrain_clip.py --clip_lr 5e-6 --train_data_type newspapers_labelled --wandb_name bienc_clip_pretrain_labelled_m3_v3_newspapers_contrastive --m 3 --training_type train_bienc --im_wt 0.5 --k 3 --supcon_temp 0.1 --train_hardneg --checkpoint "/mnt/data01/clippings_general/models/clip_pretrain_unlabelled_m1_newspapers_cc.pt" --loss_type "contrastive" --contrastive_loss_pos_margin 0.4 --contrastive_loss_neg_margin 0.9
+CUDA_VISIBLE_DEVICES=0 python clippings/continue_pretrain_clip.py --clip_lr 5e-5 --train_data_type newspapers_labelled --wandb_name bienc_clip_pretrain_labelled_m3_v3_newspapers_contrastive --m 3 --training_type train_bienc --im_wt 0.5 --k 3 --supcon_temp 0.1 --train_hardneg --checkpoint "/mnt/data01/clippings_general/models/clip_pretrain_unlabelled_m1_newspapers_cc.pt" --loss_type "contrastive" --contrastive_loss_pos_margin 0 --contrastive_loss_neg_margin 0.5
 
 
 ###Inference ARI
