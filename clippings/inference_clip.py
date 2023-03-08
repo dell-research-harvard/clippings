0836ac1c4970749a52e93a823fe9853db35c1493
econabhishek <abhishek.arora1996@gmail.com>
2023-03-07 23:59:59 -0500

    test

--- clippings/inference_clip.py
+++ clippings/inference_clip.py
@@ -275,6 +275,7 @@ if __name__ == "__main__":
     clusters=cluster("SLINK",cluster_params={"min cluster size":1,"threshold":0.9,"metric":"cosine"},corpus_embeddings=all_embeddings,corpus_ids=None)
     print("Done getting clusters")
     print(clusters)
+    print(max(cluster))
 
     print(adjusted_rand_score(clusters,all_labels))
   
