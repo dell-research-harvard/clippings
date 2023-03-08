9e3871b9f8b6eebc19d5092f2578cbd84fbd3df6
econabhishek <abhishek.arora1996@gmail.com>
2023-03-07 22:37:33 -0500

    test

--- clippings/all_commands.sh
+++ clippings/all_commands.sh
@@ -6,3 +6,5 @@ CUDA_VISIBLE_DEVICES=0 python clippings/continue_pretrain_clip.py --clip_lr 5e-6
 
 CUDA_VISIBLE_DEVICES=1 python clippings/continue_pretrain_clip.py --clip_lr 5e-9 --train_data_type newspapers_labelled --wandb_name bienc_clip_pretrain_labelled_m3_v3_newspapers_lowlr --m 3 --training_type train_bienc --im_wt 0.5 --k 3 --supcon_temp 0.1 --train_hardneg --checkpoint "/mnt/data01/clippings_general/models/clip_pretrain_unlabelled_m1_newspapers_cc.pt"
 
+
+CUDA_VISIBLE_DEVICES=1 python clippings/continue_pretrain_clip.py --clip_lr 5e-9 --train_data_type newspapers_labelled --wandb_name bienc_clip_pretrain_labelled_m3_v3_newspapers_nosingle --m 3 --training_type train_bienc --im_wt 0.5 --k 3 --supcon_temp 0.1 --train_hardneg --checkpoint "/mnt/data01/clippings_general/models/clip_pretrain_unlabelled_m1_newspapers_cc.pt"
