16212e832aac1741d2bcf31a2180cf36f889fdde
econabhishek <abhishek.arora1996@gmail.com>
2023-03-07 19:43:36 -0500

    test

--- clippings/scripts/copy_all_pulled_images.py
+++ clippings/scripts/copy_all_pulled_images.py
@@ -8,12 +8,13 @@ import glob
 
 new_folder='/mnt/data02/captions/pulled_crops_quicker_all/'
 
-list_of_images = glob.glob('/mnt/data02/captions/train_day_pulled_crops_quicker**.png')
+list_of_images = glob.glob('/mnt/data02/captions/train_day_pulled_crops_quicker**/*.png')
 
 print("Number of images", len(list_of_images))
 
-##Create new folder
-os.mkdir(new_folder)
+##Create new folder if it doesn't exist
+if not os.path.exists(new_folder):
+    os.makedirs(new_folder)
 
 for image in list_of_images:
     shutil.copy(image, new_folder)
